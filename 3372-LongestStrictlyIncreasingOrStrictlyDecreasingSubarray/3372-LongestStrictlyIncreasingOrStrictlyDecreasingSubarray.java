943be88add3b8eace7f0ca2ddc915b7bfbaa423b
Yoshwa <ryoshwaa@gmail.com>
2025-07-25 08:48:50 +0530

    Create 3372-LongestStrictlyIncreasingOrStrictlyDecreasingSubarray

---
+++ 3372-LongestStrictlyIncreasingOrStrictlyDecreasingSubarray/3372-LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.java
@@ -0,0 +1,25 @@
+// Last updated: 7/25/2025, 8:48:50 AM
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int n = nums.length;
+        int inclong = 1;
+        int declong = 1;
+        int maxlong = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i + 1] > nums[i]) {
+                inclong++;
+                declong = 1;
+            } else if (nums[i + 1] < nums[i]) {
+                declong++;
+                inclong = 1;
+            } else {
+                declong = 1;
+                inclong = 1;
+            }
+            maxlong = Math.max(maxlong, Math.max(inclong, declong));
+        }
+        return maxlong;
+
+    }
+}
