e03f6f7e94c5f2c1f5c255539be953b13efc6af3
Yoshwa <ryoshwaa@gmail.com>
2025-07-25 08:52:20 +0530

    Create 953-ReverseOnlyLetters

---
+++ 953-ReverseOnlyLetters/953-ReverseOnlyLetters.java
@@ -0,0 +1,30 @@
+// Last updated: 7/25/2025, 8:52:20 AM
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left < right) {
+            // Move left pointer to the next letter from left
+            while (left < right && !Character.isLetter(arr[left])) {
+                left++;
+            }
+
+            // Move right pointer to the next letter from right
+            while (left < right && !Character.isLetter(arr[right])) {
+                right--;
+            }
+
+            // Swap the letters
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+}
