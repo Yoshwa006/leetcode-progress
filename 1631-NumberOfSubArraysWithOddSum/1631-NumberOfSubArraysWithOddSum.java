80bbd80ba1a6adfb95def70a2e71e6d3e7406a83
Yoshwa <ryoshwaa@gmail.com>
2025-07-25 08:51:19 +0530

    Create 1631-NumberOfSubArraysWithOddSum

---
+++ 1631-NumberOfSubArraysWithOddSum/1631-NumberOfSubArraysWithOddSum.java
@@ -0,0 +1,27 @@
+// Last updated: 7/25/2025, 8:51:18 AM
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+    int mod = 1000000007; 
+    int prefixSum = 0; 
+    int even = 1; 
+    int odd = 0; 
+    int ans = 0; 
+    for(int i=0; i<arr.length; i++)
+    {
+     prefixSum += arr[i];
+     if(prefixSum%2==0)
+     {
+      ans += odd;
+      even++;  
+     }
+     else
+     {
+      ans += even;
+      odd++;  
+     }  
+      ans = ans % mod; 
+    } 
+     return ans;
+    }
+   
+}
