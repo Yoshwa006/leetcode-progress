18760c96383e47bc23b08dddc72386747bbe690d
Yoshwa <ryoshwaa@gmail.com>
2025-07-25 08:52:17 +0530

    Create 978-ValidMountainArray

---
+++ 978-ValidMountainArray/978-ValidMountainArray.java
@@ -0,0 +1,23 @@
+// Last updated: 7/25/2025, 8:52:17 AM
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int n = arr.length;
+    if (n < 3) return false;
+
+    int i = 0;
+
+    while (i < n - 1 && arr[i] < arr[i + 1]) {
+        i++;
+    }
+
+
+    if (i == 0 || i == n - 1) return false;
+
+  
+    while (i < n - 1 && arr[i] > arr[i + 1]) {
+        i++;
+    }
+
+    return i == n - 1;
+    }
+}
