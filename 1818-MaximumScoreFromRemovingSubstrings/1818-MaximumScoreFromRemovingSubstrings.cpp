bdf1dfcad5a5e7cafef72d98832ab4bf07a64e7b
Yoshwa <ryoshwaa@gmail.com>
2025-07-25 08:50:57 +0530

    Create 1818-MaximumScoreFromRemovingSubstrings

---
+++ 1818-MaximumScoreFromRemovingSubstrings/1818-MaximumScoreFromRemovingSubstrings.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/25/2025, 8:50:57 AM
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        if (x<y){
+            reverse(s.begin(), s.end());
+            swap(x, y);
+        }
+        int a=0, b=0, points=0;
+        for(char c: s){
+            switch(c){
+                case 'a': a++; break;
+                case 'b':
+                    if (a>0){
+                        a--;
+                        points+=x;
+                    }
+                    else b++;
+                    break;
+                default:
+                    points+=min(a, b)*y;
+                    a=b=0;
+            }
+        }
+        points+=min(a, b)*y;
+        return points;
+    }
+};
