7204b1fae9b3b64585323d44b72fc84cc409dcd1
Yoshwa <ryoshwaa@gmail.com>
2025-07-26 14:48:08 +0530

    Create 2767-MaximumSumWithExactlyKElements

---
+++ 2767-MaximumSumWithExactlyKElements/2767-MaximumSumWithExactlyKElements.java
@@ -0,0 +1,17 @@
+// Last updated: 7/26/2025, 2:48:08 PM
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        int max = 0;
+        for(int i = 0; i<nums.length; i++){ 
+            max = Math.max(max, nums[i]);
+        }
+        
+        int ans = 0;
+        while(k>0){
+            ans+=max;
+            max = max+1;
+            k--;
+        }
+        return ans;
+    }   
+}
