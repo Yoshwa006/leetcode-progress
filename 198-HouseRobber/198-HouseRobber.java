06f6dcd19b52edb93b386d877d1c1c16a2b31a09
Yoshwa <ryoshwaa@gmail.com>
2025-07-29 22:41:59 +0530

    File updated at 7/29/2025, 10:41:59 PM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,19 +1,24 @@
-// Last updated: 7/25/2025, 8:53:55 AM
+// Last updated: 7/29/2025, 10:41:59 PM
 class Solution {
     public int rob(int[] nums) {
-         int n = nums.length;
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-
+        int n = nums.length;
         int[] dp = new int[n];
-
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-
-        return dp[n - 1];
+        Arrays.fill(dp, -1);
+        return houseRobber(n-1, nums, dp);
     }
+
+    public int houseRobber(int index, int[] arr, int[] dp) {
+        if (index < 0)
+            return 0;
+
+        if (dp[index] != -1)
+            return dp[index];
+
+        int rob = arr[index] + houseRobber(index - 2, arr, dp);
+        int skip = houseRobber(index - 1, arr, dp);
+
+        dp[index] = Math.max(rob, skip);
+        return dp[index];
+    }
+
 }
