1a3ee6bcf5f923560c30bc8cded60f3f816e7fa7
Yoshwa <ryoshwaa@gmail.com>
2025-08-14 18:13:29 +0530

    File updated at 8/14/2025, 6:13:28 PM

--- 2264-Largest3-Same-DigitNumberinString/2264-Largest3-Same-DigitNumberinString.java
+++ 2264-Largest3-Same-DigitNumberinString/2264-Largest3-Same-DigitNumberinString.java
@@ -1,15 +1,13 @@
-// Last updated: 8/14/2025, 6:12:08 PM
+// Last updated: 8/14/2025, 6:13:28 PM
 class Solution {
-    public String largestGoodInteger(String s) {
-        String maxStr = "";
-        for (int i = 0; i <= s.length() - 3; i++) {
-            String sub = s.substring(i, i + 3);
-            if (sub.charAt(0) == sub.charAt(1) && sub.charAt(1) == sub.charAt(2)) {
-                if (maxStr.isEmpty() || sub.compareTo(maxStr) > 0) {
-                    maxStr = sub;
-                }
-            }
+    public String largestGoodInteger(String num) {
+     String[] numbers={"999","888","777","666","555","444","333","222","111","000"};
+     for(String i:numbers){
+        if(num.contains(i)){
+            return i;
         }
-        return maxStr;
+     }
+     return "";
+
     }
 }
