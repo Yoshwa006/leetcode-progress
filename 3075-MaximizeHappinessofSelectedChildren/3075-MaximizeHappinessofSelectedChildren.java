bdb6033451143e927053a9299e40c700538d5389
Yoshwa <ryoshwaa@gmail.com>
2025-08-15 13:56:31 +0530

    File updated at 8/15/2025, 1:56:31 PM

--- 3075-MaximizeHappinessofSelectedChildren/3075-MaximizeHappinessofSelectedChildren.java
+++ 3075-MaximizeHappinessofSelectedChildren/3075-MaximizeHappinessofSelectedChildren.java
@@ -1,24 +1,21 @@
-// Last updated: 8/15/2025, 1:56:02 PM
+// Last updated: 8/15/2025, 1:56:31 PM
 class Solution {
-    public long maximumHappinessSum(int[] arr, int k) {
-        int n = arr.length;
-        long step = 0;
-        long score = 0;
-
-        Arrays.sort(arr);
-
-        int r = n;
-        while(k > 0){
-            long curr = arr[r-1]  -step;
-            if(curr < 0){
-                return score;
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        long res = 0;
+        long minus = 0;
+        int right = happiness.length-1;
+        while(k>0){
+            if(happiness[right] - minus > 0 ){
+                res += happiness[right] - minus;
+                right--;
+                minus++;
+                k--;
+            }else{
+                return res;
             }
-            score += arr[r-1] - step;
-            step++;
-            k--;
-            r--;
+            
         }
-        
-        return score;
+        return res;
     }
 }
